0d6557d30f165817066d8e1c72278b06e6a1b031
Michael Schmidt <schmidt.m.dev@gmail.com>
2025-05-13 22:10:08 +0200

    initilized project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+import { Counter } from "./features/counter/Counter"
+import { Quotes } from "./features/quotes/Quotes"
+
+export const App = () => (
+  <div className="App">
+    <Button>Test</Button>
+    {/* <Counter />
+    <Quotes /> */}
+  </div>
+)
